2b79f988220f0773dd46e1fc979249c2cc4a8a18
kartik kashyap <kashyapkartik1022007@gmail.com>
2026-09-20 09:58:19 +0530

    day7[align frontend with backend]

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
   return (
   
     <>
-    <Regestration/>
+    <Login/>
     
     </>
     
--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Sparkles } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 
-const login = () => {
+const Login = () => {
+const{loading, handleLogin} = useAuth()
+const [email, setEmail] = useState("")
+const [password, setPassword] = useState("")
+
+const handleSubmit = async(e)=>{
+  e.preventDefault()
+  handleLogin({email,password})
+}
+if(loading){
+  return(<main><h1>loadinggg...</h1></main>)
+}
   return (
       <div>
       <div id="login_page" className='bg-[#FFF7ED] h-screen overflow-hidden flex  '>
@@ -18,26 +29,39 @@ const login = () => {
                   <div id="dots"><div id="circle" className='h-2.5 shadow-[2px_2px_0px_0px_#000000] border-2 w-2.5 rounded-[50%]'></div></div>
                   <div id="dots"><div id="circle" className='h-2.5  shadow-[2px_2px_0px_0px_#000000] border-2 w-2.5 rounded-[50%]'></div></div>
           </div>
-                 
-                  <div id="star " className='shadow-[4px_4px_0px_0px_#000000] border-2  mb-[5%] rounded-xl bg-[#ff6482] p-3 '><Sparkles /></div>
+                <form onSubmit={handleSubmit}>
+                  
+                  
+                   <div id="star " className='shadow-[4px_4px_0px_0px_#000000] border-2  mb-[5%] rounded-xl bg-[#ff6482] p-3 '><Sparkles /></div>
                   <div id="login_box" className='border p-4  rounded-xl flex flex-col shadow-[8px_8px_0px_0px_#000000] items-center justify-start w-[30vw]  bg-white'>
                     <pre className='font-bold mt-[3%] text-[3vw] p-3'>Welcome back</pre>
                     <p className='text-gray-600 -mt-3 pb-9'>Log in to keep building. </p>
                     <div id="inputs" className=' w-[70%] p-1 flex gap-0.5 flex-col items-center justify-center  '>
                       <p className='self-start  font-bold  text font-mono capitalize'>email</p>
-                    <input className= ' w-full rounded-sm border p-1 ' type="email" placeholder='enter email' />
+              <input onChange={(e) => {
+                setEmail(e.target.value)
+              }}
+                     className= ' w-full rounded-sm border p-1 ' type="email" value={email} placeholder='enter email' />
                     <p className='self-start  font-bold mt-3 text font-mono capitalize'>password</p>
-                    <input className=' w-full rounded-sm p-1 border  ' type="password"  placeholder='password'/>
+                    <input
+                onChange={(e) => {
+                  setPassword(e.target.value)
+                }}
+                     className=' w-full rounded-sm p-1 border  ' value={password} type="password"  placeholder='password'/>
 
-                    <p className='w-full' to='/home' ><button className='w-full rounded-sm bg-[#FF6482] active:bg-[#ffffff]    active:shadow-none p-1 mt-8 font-mono  border shadow-[4px_4px_0px_0px_#000000]'
+                    <p className='w-full' to='/home' ><button 
+                    onClick={handleSubmit}
+                    type='submit' className='w-full rounded-sm bg-[#FF6482] active:bg-[#ffffff]    active:shadow-none p-1 mt-8 font-mono  border shadow-[4px_4px_0px_0px_#000000]'
                     >Login</button></p>
                     <p className='p-3 font-bold'>OR</p>
                     <button className=' rounded-sm bg-[#B28BFF] active:bg-[#ffffff]    active:shadow-none p-1  font-mono w-full border shadow-[4px_4px_0px_0px_#000000]'>//Continue with gitHub</button>
                     <pre className='p-3'>New here/- <span className='text-[#0f7bbd] font-bold hover:underline'>create an account</span></pre>
                     </div>
+                      </div>
+                  </form> 
+                 
                     
-                    
-                  </div>
+                
 
         </div>
         <div id="right" className=' flex-1 '></div>
@@ -46,4 +70,4 @@ const login = () => {
   )
 }
 
-export default login
+export default Login
