47de4dfbd09c9ef4e795608238c4ae70bc45a8e5
kartik kashyap <kashyapkartik1022007@gmail.com>
2026-09-20 09:56:11 +0530

    day7[cors setup]

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,14 @@
 import express from "express"
 import route from "./routes/registere.user.js";
 import cookieParser from "cookie-parser";
-
+import cors from "cors"
 const app = express();
 app.use(express.json());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials: true
+}))
 app.use(cookieParser())
-app.use("/api/register", route)
+app.use("/api/auth", route)
 
 export default app;
